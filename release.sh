0739e15e8493e4b32d179daa13d54f17570f83b7
tsar-boomba <itg.2048@gmail.com>
2024-04-26 03:47:29 -0400

    chore improve release script

--- release.sh
+++ release.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+# Check if there are any staged changes
+if [[ -n $(git diff --cached --exit-code) ]]; then
+    echo "Staged changes detected. Commit before release."
+	exit 1
+fi
+
+# Check if there are any unstaged changes
+if [[ -n $(git status --porcelain) ]]; then
+    echo "Unstaged changes detected. Commit before release."
+	exit 1
+fi
+
+git cliff > CHANGELOG.md
+git commit -m "chore(release) prepare for release"
 cargo release --tag-name 'v{{version}}' -v $@
