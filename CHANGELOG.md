0026e08cbbf5e4dd09cbbef28b5dd028f6a81b07
tsar-boomba <itg.2048@gmail.com>
2024-04-26 04:04:27 -0400

    feat(release) add musl targets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.0.6] - 2024-04-26
+
+### ⚙️ Miscellaneous Tasks
+
+- Release soft-serve version 0.0.6
+
 ## [0.0.5] - 2024-04-26
 
 ### ⚙️ Miscellaneous Tasks
--- release.sh
+++ release.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-# Check if there are any staged changes
-if [[ -n $(git diff --cached --exit-code) ]]; then
-    echo "Staged changes detected. Commit before release."
-	exit 1
-fi
-
-# Check if there are any unstaged changes
-if [[ -n $(git status --porcelain) ]]; then
-    echo "Unstaged changes detected. Commit before release."
-	exit 1
-fi
-
-git-cliff -o CHANGELOG.md
-git commit -am "chore(release) prepare for release"
-cargo release --tag-name 'v{{version}}' -v -x $@
+cargo release --tag-name 'v{{version}}' -v $@
